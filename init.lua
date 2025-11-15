3a87fcc0f090c8e762668f6735767481cab81543
DavidJ4 <davidj4tech@gmail.com>
2025-11-15 22:17:39 +1100

    Removed background colors for transparency

--- init.lua
+++ init.lua
@@ -1014,3 +1014,17 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Mods made by davidj4tech ~/.config/nvim/init.lua
+
+-- Transparent background
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NonText', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
+-- Minimal UI: hide statusline and cmdline when not in use
+vim.o.laststatus = 1
+vim.o.cmdheight = 0
+
