571670534f8ff2639db62c25f958d23c9be2108c
DavidJ4 <davidj4tech@gmail.com>
2026-04-19 21:03:47 +1000

    fix: use current nvim-treesitter install API

--- init.lua
+++ init.lua
@@ -853,7 +853,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     config = function()
       local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(filetypes)
+      require('nvim-treesitter.install').ensure_installed(filetypes)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
         callback = function() vim.treesitter.start() end,
